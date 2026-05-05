5b2fdb54e3e98848751542652cc2eed6cccdd0fe
Shammasss <160420032+Chammx@users.noreply.github.com>
2026-05-05 12:37:41 +0530

    fix import error

--- app.py
+++ app.py
@@ -1,5 +1,26 @@
-if st.button("Run Strategy"):
+import streamlit as st
 
+st.title("🏁 STRAT-AI")
+
+def tire_degradation(tire_age):
+    return 0.05 * tire_age
+
+def simulate_stay_out(tire_age):
+    total = 0
+    for i in range(5):
+        total += 90 + tire_degradation(tire_age + i)
+    return total
+
+def simulate_pit(pit_loss):
+    total = pit_loss
+    for i in range(5):
+        total += 90
+    return total
+
+tire_age = st.slider("Tire Age", 0, 30, 15)
+pit_loss = st.slider("Pit Loss", 15, 30, 20)
+
+if st.button("Run Strategy"):
     stay = simulate_stay_out(tire_age)
     pit = simulate_pit(pit_loss)
 
